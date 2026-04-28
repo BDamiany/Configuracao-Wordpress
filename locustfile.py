fe8fe8caa2b649bff958cf5c6b26829453e4cc6d
Brenno Damiany <damianybrenno@gmail.com>
2026-04-28 17:56:27 -0300

    Update URLs in locustfile.py for testing

--- locustfile.py
+++ locustfile.py
@@ -4,15 +4,15 @@ class UserTasks(TaskSet):
 
     @task(1)
     def post_imagem_1mb(self):
-        self.client.get("/2026/04/21/teste-imagem-1mb/")
+        self.client.get("/2026/04/28/imagem-1mb/")
 
     @task(1)
     def post_texto_400kb(self):
-        self.client.get("/2026/04/21/teste-texto-400kb/")
+        self.client.get("/2026/04/28/texto-400kb/")
 
     @task(1)
     def post_imagem_300kb(self):
-        self.client.get("/2026/04/21/teste-imagem-300kb/")
+        self.client.get("/2026/04/28/imagem-300kb/")
 
 class WebsiteUser(HttpUser):
     tasks = [UserTasks]
